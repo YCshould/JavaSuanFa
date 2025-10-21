7e561f2628ec91d2eb1f783d9b2bc87ca5e08fda
YCshould <2725687757@qq.com>
2025-10-21 11:24:52 +0800

    branch_2第一次提交

--- src/Main2.java
+++ src/Main2.java
@@ -1,5 +1,7 @@
 public class Main2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("这是分支二");
+        System.out.println("这是分支二的第二行");
     }
 }
