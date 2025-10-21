3c35e59163231b1d81aa824ddefce4e36359c49d
YCshould <2725687757@qq.com>
2025-10-21 11:10:18 +0800

    自定义合并master和branch_!

--- src/Main2.java
+++ src/Main2.java
@@ -1,5 +1,7 @@
 public class Main2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("This is Main2.java");
+        System.out.println("This is the second file in the project.");
     }
 }
